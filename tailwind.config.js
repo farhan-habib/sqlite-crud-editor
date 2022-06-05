46d356c5993899fca9fec6aa6cd9cd48eb3096a1
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-06-04 23:07:30 -0700

    Installing and setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	mode: "jit",
+	purge: ["./public/index.html", "./public/404.html", "./src/**/*.{vue,js}"],
+	darkMode: true,
+	theme: {
+		extend: {},
+	},
+	// content: [],
+	plugins: [],
+};
