b147d50fd3aa76c1a6ac99a5c20a997a903168a7
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-06-05 00:32:00 -0700

    Using @ instead of relative path when getting tailwind css

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
-import "./assets/tailwind.css";
+import "@/assets/tailwind.css";
 
 import "flowbite";
 // import PrimeVue from "primevue/config";
