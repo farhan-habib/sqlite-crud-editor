23133ce5df293abcb4b423afce3e3ac9d27c3150
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-06-05 21:13:16 -0700

    Working on theming the site a bit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <script setup></script>
 
 <template>
-	<div style="margin: 10%; border: 5px solid #1c6ea4">
-		<Suspense>
-			<router-view />
-		</Suspense>
-	</div>
+	<!-- <div style="margin: 10%; border: 5px solid #1c6ea4"> -->
+	<Suspense>
+		<router-view />
+	</Suspense>
+	<!-- </div> -->
 </template>
 
 <style lang="scss">
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -56,41 +56,50 @@ function handleDatabase() {
 </script>
 
 <template>
+	<div id="buttonFlex">
+		<div class="flex flex-row justify-between">
+			<div class="inline-flex rounded-md shadow-sm" role="group">
+				<button type="button" @click="res.open()"
+					class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
 
-	<div class="flex flex-row justify-between">
-		<div class="inline-flex rounded-md shadow-sm" role="group">
-			<button type="button" @click="res.open()"
-				class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+						<path fill-rule="evenodd"
+							d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 10-2 0v1H8a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V9z" />
+					</svg>
+					Open
+				</button>
+				<button type="button" @click="res.saveAs"
+					class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+						<path fill-rule="evenodd"
+							d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+							clip-rule="evenodd" />
+					</svg>
+					Download
+				</button>
+			</div>
 
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-					<path fill-rule="evenodd"
-						d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 10-2 0v1H8a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V9z" />
-				</svg>
-				Open
-			</button>
-			<button type="button" @click="res.saveAs"
-				class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-					<path fill-rule="evenodd"
-						d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-						clip-rule="evenodd" />
-				</svg>
-				Download
-			</button>
-		</div>
-
-		<div class="inline-flex rounded-md shadow-sm" role="group">
-			<button type="button" @click="onSave"
-				class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700">
-				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-					<path
-						d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z" />
-				</svg>
-				Update Local File
-			</button>
+			<div class="inline-flex rounded-md shadow-sm" role="group">
+				<button type="button" @click="onSave"
+					class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700">
+					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+						<path
+							d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z" />
+					</svg>
+					Update Local File
+				</button>
+			</div>
 		</div>
 	</div>
 
+	<splitpanes class="default-theme" vertical>
+		<pane size="50">
+			<h>View Database</h>
+		</pane>
+		<pane size="50">
+			<h>SQL Commands</h>
+		</pane>
+	</splitpanes>
 
 	<hr />
 	<button @click="handleDatabase">Handle Database</button>
