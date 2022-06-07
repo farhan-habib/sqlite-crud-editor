602ac40cbc919c12124fefa9a723fa7a8d6488dd
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-06-07 15:13:28 -0700

    adding link to go to test page from main

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,10 @@
 <script setup>
 //https://vuejsexamples.com/a-easy-to-use-data-table-component-made-with-vue-js-3-x
-const headers = [
-	{ text: "Name", value: "name" },
-	{ text: "Height (cm)", value: "height", sortable: true },
-	{ text: "Weight (kg)", value: "weight", sortable: true },
-	{ text: "Age", value: "age", sortable: true }
-];
 
-const items = [
-	{ "name": "Curry", "height": 178, "weight": 77, "age": 20 },
-	{ "name": "James", "height": 180, "weight": 75, "age": 21 },
-	{ "name": "Jordan", "height": 181, "weight": 73, "age": 22 }
-];
 </script>
 
 <template>
-	<EasyDataTable :headers="headers" :items="items" />
+	<router-link to="/test">CLICK HERE TO GO TO TEST PAGE</router-link>
 	<!-- <splitpanes class="default-theme" horizontal style="width:100%; height:20em">
 		<pane size="65">
 			<span>1</span>
