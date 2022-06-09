9a9ecc32778efd21056e4434dad9a206a1a2ea93
Farhan Habib <85850125+f-habibCA@users.noreply.github.com>
2022-06-09 15:19:52 -0700

    More changes to the top menu bar of the webpage

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -62,53 +62,50 @@ async function newDatabase() {
 </script>
 
 <template>
-	<div id="buttonFlex">
-		<div class="flex flex-row justify-between">
-			<div id="fileUpDwn" class="inline-flex rounded-md shadow-sm" role="group">
-				<button type="button" @click="newDatabase"
-					class=" inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
+	<header>
+		<!-- <div id="menuBar" class="flex flex-row justify-between
+	bg-gray-300"> -->
+		<div id="fileUpDwn" class="inline-flex rounded-md shadow-sm" role="group">
+			<button type="button" @click="newDatabase" class="">
 
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-						<path fill-rule="evenodd"
-							d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 10-2 0v1H8a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V9z" />
-					</svg>
-					Open
-				</button>
-				<button type="button" @click="FileSystemAccessHelper.saveAsNew" :disabled="content == undefined"
-					class=" inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md border border-gray-900 hover:bg-gray-900 hover:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 ">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-						<path fill-rule="evenodd"
-							d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-							clip-rule="evenodd" />
-					</svg>
-					Download
-				</button>
-			</div>
-
-			<div id="dbManipulation" class="inline-flex rounded-md shadow-sm" role="group">
-				<button type="button" @click="executeUserQuery" :disabled="content == undefined"
-					class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-						<path fill-rule="evenodd"
-							d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-							clip-rule="evenodd" />
-					</svg>
-					Run Command
-				</button>
-			</div>
-
-			<div id="fileSaveLocal" class="inline-flex rounded-md shadow-sm" role="group">
-				<button type="button" @click="FileSystemAccessHelper.updateLocal" :disabled="content == undefined"
-					class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md rounded-l-lg border border-gray-900 hover:bg-gray-900 hover:text-white  dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700">
-					<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-						<path
-							d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z" />
-					</svg>
-					Update Local File
-				</button>
-			</div>
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+					<path fill-rule="evenodd"
+						d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 10-2 0v1H8a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V9z" />
+				</svg>
+				Open
+			</button>
+			<button type="button" @click="FileSystemAccessHelper.saveAsNew" :disabled="content == undefined" class="">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+					<path fill-rule="evenodd"
+						d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+						clip-rule="evenodd" />
+				</svg>
+				Download
+			</button>
 		</div>
-	</div>
+
+		<div id="dbManipulation" class="inline-flex rounded-md shadow-sm" role="group">
+			<button type="button" @click="executeUserQuery" :disabled="content == undefined" class="">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+					<path fill-rule="evenodd"
+						d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+						clip-rule="evenodd" />
+				</svg>
+				Run Command
+			</button>
+		</div>
+
+		<div id="fileSaveLocal" class="inline-flex rounded-md shadow-sm" role="group">
+			<button type="button" @click="FileSystemAccessHelper.updateLocal" :disabled="content == undefined" class="">
+				<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+					<path
+						d="M7.707 10.293a1 1 0 10-1.414 1.414l3 3a1 1 0 001.414 0l3-3a1 1 0 00-1.414-1.414L11 11.586V6h5a2 2 0 012 2v7a2 2 0 01-2 2H4a2 2 0 01-2-2V8a2 2 0 012-2h5v5.586l-1.293-1.293zM9 4a1 1 0 012 0v2H9V4z" />
+				</svg>
+				Update Local File
+			</button>
+		</div>
+		<!-- </div> -->
+	</header>
 
 	<splitpanes id="contentPane" class="splitPaneTheme" horizontal>
 
@@ -148,7 +145,18 @@ async function newDatabase() {
 	@apply h-auto
 }
 
-:disabled {
-	@apply border-solid border-2 border-sky-500
+button {
+	@apply inline-flex items-center py-2 px-4 text-sm font-medium text-gray-900 bg-transparent rounded-r-md rounded-l-lg border border-gray-900;
+
+	&:not([disabled]) {
+		&:hover {
+			@apply bg-gray-900 text-white
+		}
+	}
+
+	&:disabled {
+		@apply opacity-30;
+	}
+
 }
 </style>
